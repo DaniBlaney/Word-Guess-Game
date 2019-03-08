943e0f93ce15d1377707ccf288dfb41ebdaab303
Danielle Blaney <daniblaney@gmail.com>
2019-03-08 16:44:26 -0500

    third commit

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -9,7 +9,7 @@ var losses = document.getElementById('losses');
 
 
 //Create variables (words, wins, losses, incorrect letters)
-var characterNameBank = ["Darth Vader", "Princess Leia", "Luke Skywalker", "Obi-Wan Kenobi", "Han Solo", "Chewbacca", "R2-D2", "C-3PO", "Greedo", "Boba Fett", "Jabba the Hutt", "Jar Jar Binks", "Yoda"];
+var characterNameBank = ["Darth Vader", "Princess Leia", "Luke Skywalker", "Obi-Wan Kenobi", "Han Solo", "Chewbacca", "Greedo", "Boba Fett", "Jabba the Hutt", "Jar Jar Binks", "Yoda"];
 var guessesLeft = 25;
 var pickedCharacterName = '';
 var gameRunning = false;
@@ -47,53 +47,86 @@ guessedLetters.textContent = incorrectLetterBank;
 }
 
 
-// letterguess function, letter pressed and returns letter in incorrect or correct on keyup
+// letter guess function, letter pressed and returns letter in incorrect or correct on keyup
 function letterGuess(letter) {
   console.log(letter);
 
   if (gameRunning === true && guessedLetterBank.indexOf(letter) === -1) {
     guessedLetterBank.push(letter);
-
-    for var i = 0; i < pickedCharacterName.length; i++) {
+//checks if letter is in my character name
+    for (var i = 0; i < pickedCharacterName.length; i++) {
       if (pickedCharacterName[i].toLowerCase() === letter.toLowerCase()) {
         pickedWordPlaceholderArr[i] = pickedCharacterName[i];
       }
     }
 
   placeholders.textContent = pickedWordPlaceholderArr.join('');
+  checkIncorrect(letter);
 
   }
   else {
     if (!gameRunning){
       alert("click new game");
-      else {
+    } else {
         alert("you've already guessed this letter");
       }
     }
+
   }
 
 
-//check losses
-
+//check losses..
+function checkLoss() {
+  if (guessesLeft === 0){
+    losses++;
+    gameRunning=false;
+    losses.textContent = losses;
+    placeholders.textContent= pickedCharacterName
+  }
+}
+checkWin();
 //check wins
+function checkWin() {
+  if (pickedCharacterName.toLowerCase()=== pickedWordPlaceholderArr.join('').toLowerCase)
+{
+  wins++;
+  gameRunning=false;
+  wins.textContent=wins;
+}
+}
 
 //incorrect letter
 function checkIncorrect(letter) {
   if (pickedWordPlaceholderArr.indexOf(letter.toLowerCase()) === -1 &&
-  pickedWordPlaceholderArr.indexOf(letter.toUppercase()) === -1)
+  pickedWordPlaceholderArr.indexOf(letter.toUppercase()) === -1);
   {
     //decreases guesses
-    guessesLeft==;
+    guessesLeft --;
     //adds the incorrect letter to bank
     incorrectLetterBank.push(letter);
-    guessedLetters.textContent = incorrectLetterBank.join(' ');
+    guessedLetters.textContent = incorrectLetterBank.join('');
     //writes how many guesses left to dom
     guessesLeft.textContent = guessesLeft;
   }
+
+  //play sound when letters are guessed
+function playSound(soundfile){
+  document.getElementById("sound").innerHTML="<embed src=\""+soundfile+"\" hidden=\"true\" autostart=\"true\" loop=\"false\"/>";
+}
+
+if(guessedLetterBank){// Your condition
+  playSound("./assets/images/yodalaughing.mp3"); // Location to your sound file
+}
+
+if(incorrectLetterBank){// Your condition
+  playSound("./assets/images/dontdothat.mp3"); // Location to your sound file
 }
 
 
-//new game button event listener...WHY WONT THIS WORKKKKKKK
+}
+checkLoss();
+
+//new game button event listener...
 newGameButton.addEventListener('click', newGame);
 
 document.onkeyup = function(event) {
